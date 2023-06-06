36b57d041d67a63299526cf35ab256f6298b31dd
Jeff Rossi <jrossi@intouchgps.com>
2023-06-06 09:40:09 -0400

    initial

---
+++ src/lib/api-url.ts
@@ -0,0 +1,1 @@
+export const apiUrl = 'https://issue-tracker-api.jeff-rossi.com/';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex flex-wrap justify-between mx-4 my-4">
+	<a href="/">Home</a>
+	<a href="/dashboard">Dashboard</a>
+	<a href="/projects">Projects</a>
+	<a href="/issues">Issues</a>
+	<a href="/users">Users</a>
+	<a href="/time_clocks">Time Clocks</a>
+</div>
+
+<slot />
